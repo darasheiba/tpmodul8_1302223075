8f55bc9bee13116830c8d19a85f947dc879eaa9a
darasheiba <darasheibaa@gmail.com>
2024-04-15 00:18:49 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_130222305/CovidConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Internal;
+using System.IO;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace tpmodul8_130222305
+{
+	public class CovidConfig
+	{
+        public string satuanSuhu { get; set; }
+        public int batasHarianDemam { get; set; }
+        public string pesanDitolak { get; set; }
+        public string pesanDiterima { get; set; }
+
+        public void UbahSatuan()
+        {
+            satuanSuhu = satuanSuhu == "celsius" ? "fahrenheit" : "celsius";
+        }
+    }
+}
+
+
--- tpmodul8_130222305/Program.cs
+++ tpmodul8_130222305/Program.cs
@@ -1,3 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using Internal;
+using System.IO;
+using System.Text.Json;
+using Newtonsoft.Json;
+using tpmodul8_130222305;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string jsonFilePath = "/Users/darasheibamalikachoiriyyah/Desktop/✧.* /KPL/tpmodul8_130222305/tpmodul8_130222305/CovidConfig.json";
+        CovidConfig config;
+
+        using (FileStream fs = File.OpenRead(jsonFilePath))
+        {
+            config = JsonSerializer.Deserialize<CovidConfig>(fs);
+        }
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.SatuanSuhu}");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hariDemam = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuValid = false;
+        bool hariDemamValid = false;
+
+        if (config.SatuanSuhu == "celsius")
+            suhuValid = suhu >= 36.5 && suhu <= 37.5;
+        else if (config.satuanSuhu == "fahrenheit")
+            suhuValid = suhu >= 97.7 && suhu <= 99.5;
+
+        hariDemamValid = hariDemam < config.batasHarianDemam;
+
+        if (suhuValid && hariDemamValid)
+            Console.WriteLine(config.PesanDiterima);
+        else
+            Console.WriteLine(config.PesanDitolak);
+
+        config.UbahSatuan();
+
+        using (FileStream fs = File.OpenWrite(jsonFilePath))
+        {
+            JsonSerializer.SerializeAsync<CovidConfig>(fs, config);
+        }
+    }
+}
 
